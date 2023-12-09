bbc66329ba10cf0a219903328395b47be1ac4c94
Jimi Riihiluoma <jimi.riihiluoma@tuni.fi>
2023-12-09 22:43:18 +0200

    Add tests for toString function

--- test/toString.test.js
+++ test/toString.test.js
@@ -1,5 +1,44 @@
 import toString from "../src/toString"
 
-test("Converts integer 123 to string", () => {
+test("returns short string without modifying it", () => {
+    expect(toString("foo")).toBe("foo");
+});
+
+test("returns long string without modifying it", () => {
+    expect(toString("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."))
+    .toBe("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.");
+});
+
+test("converts symbol xyz to string \"Symbol(xyz)\"", () => {
+    const symbol = Symbol("xyz");
+    expect(toString(symbol)).toBe("Symbol(xyz)");
+  });
+
+test("converts integer 123 to string \"123\"", () => {
   expect(toString(123)).toBe("123");
+});
+
+test("converts number 0.05 to string \"0.05\"", () => {
+    expect(toString(0.05)).toBe("0.05");
+});
+  
+test("converts NaN to string \"NaN\"", () => {
+    expect(toString(NaN)).toBe("NaN");
+});
+
+test("converts array [0, 1, 2, 3] to string \"0,1,2,3\"", () => {
+    expect(toString([0, 1, 2, 3])).toBe("0,1,2,3");
+});
+
+test("converts booleans to strings", () => {
+    expect(toString(true)).toBe("true");
+    expect(toString(false)).toBe("false");
+});
+
+test("converts undefined to empty string", () => {
+    expect(toString(undefined)).toBe("");
+});
+
+test("converts null to empty string", () => {
+    expect(toString(null)).toBe("");
 });
